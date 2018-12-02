87de8184f78679c8629f02fe1aaaa717111f593e
Wilson Wang <wilsonny371@gmail.com>
2018-12-01 22:08:03 -0800

    using scripts

---
+++ project_template/scripts/find_file.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# find_file.sh <search_root_dir> <partial_name> [exe, dir, ]
+
+if [ $# -ne 3 ]
+then
+    exit 1
+fi
+
+files=$(find "$1" -name "*$2*")
+if [ "$3" == "dir" ]
+then
+    target="directory"
+elif [ "$3" == "exe" ]
+then
+    target="executable"
+fi
+if [ "${target}" == "" ]
+then
+    exit 1
+fi
+for i in ${files}
+do
+    tmptype=$(file $i)
+    if [[ "${tmptype}" == *${target}* ]]
+    then
+        echo $i
+        break
+    fi
+done
