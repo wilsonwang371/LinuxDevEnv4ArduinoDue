9900cb75ce6555858c6d6cd63aaea6b205e86f93
Wilson Wang <wilsonny371@gmail.com>
2018-12-06 14:33:11 -0800

    small fix

--- tpl/cmake_include/toolchain.cmake
+++ tpl/cmake_include/toolchain.cmake
@@ -7,16 +7,16 @@ OUTPUT_VARIABLE CMAKE_C_COMPILER)
 execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-gcc$ exe"
 OUTPUT_VARIABLE CMAKE_CXX_COMPILER)
 
-execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-ar exe"
+execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-ar$ exe"
 OUTPUT_VARIABLE CMAKE_AR)
 
-execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-as exe"
+execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-as$ exe"
 OUTPUT_VARIABLE CMAKE_ASM_COMPILER)
 
-execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-objcopy exe"
+execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*-objcopy$ exe"
 OUTPUT_VARIABLE OBJCP)
 
-execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*flash.ld file"
+execute_process (COMMAND bash -c "${CMAKE_SOURCE_DIR}/scripts/find_file.sh \"${ARDUINO_PKG_DIR}\" .*flash.ld$ file"
 OUTPUT_VARIABLE LNK_SCRIPT)
 
 message ("    GCC: ${CMAKE_C_COMPILER}")
